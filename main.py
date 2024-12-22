06f8ece2419f48481a2a4fde2fea75174bec4651
httpswnxx <zswerdx77@gmail.com>
2024-12-22 12:06:42 +0500

    id sender bot

--- main.py
+++ main.py
@@ -1,98 +1,30 @@
-from dataclasses import dataclass
-import psycopg2
+# pip install aiogram
+import asyncio
+import logging
+import sys
+from os import getenv
+
+from aiogram import Dispatcher, Bot, F
+from aiogram.filters import CommandStart
+from aiogram.types import Message
+
+TOKEN = "7750209470:AAHW9ah3xzQa8v6lbo8GVUUXll5Ia5eyYdQ"
+dp = Dispatcher()
 
 
-class PGConf:
-    DBNAME = " "
-    USER = " "
-    PASSWORD = " "
-    HOST = " "
-    PORT = "ur port"
-    con = psycopg2.connect(dbname=DBNAME, user=USER, password=PASSWORD, host=HOST, port=PORT)
-    cur = con.cursor()
+@dp.message(CommandStart())
+async def start_handler(message: Message):
+    await message.reply("Hello")
+    user_id = message.from_user.id
+    await message.answer(f"Your id is {user_id}")
 
 
-class PG(PGConf):
 
-    def generate_conditions(self, conditions_attr):
-        conditions_format = " where " + " = %s and ".join(conditions_attr.keys()) + " = %s" if conditions_attr else ""
-        return conditions_format
+async def main():
+    bot = Bot(TOKEN)
+    await dp.start_polling(bot)
 
-    def save(self):
-        table_name = self.__class__.__name__.lower() + "s"
-        attr = self.__dict__
-        [attr.pop(field) for field, value in attr.copy().items() if value == None]
-        cols = " , ".join(attr.keys())
-        format = " , ".join(["%s"] * len(attr.keys()))
-        args = tuple(attr.values())
-        query = f"""
-            insert into {table_name} ({cols}) values ({format})
-        """
-        self.cur.execute(query, args)
-        self.con.commit()
 
-    def objects(self, *columns):
-        table_name = self.__class__.__name__.lower() + "s"
-        cols = " , ".join(columns) if columns else "*"
-        conditions_attr = self.__dict__
-        [conditions_attr.pop(field) for field, value in conditions_attr.copy().items() if value == None]
-        args = tuple(conditions_attr.values())
-        query = f"""
-            select {cols} from {table_name} {self.generate_conditions(conditions_attr)}
-        """
-
-        self.cur.execute(query, args)
-        data: list[dict] = self.to_dict()
-        objects = []
-        for i in data:
-            objects.append(self.__class__(**i))
-        return objects
-
-    def to_dict(self) -> list[dict]:
-        columns = list(self.cur.description)
-        result = self.cur.fetchall()
-        results = []
-        for row in result:
-            row_dict = {}
-            for i, col in enumerate(columns):
-                row_dict[col.name] = row[i]
-            results.append(row_dict)
-        return results
-
-    def delete(self, *cols):
-        table_name = self.__class__.__name__.lower() + "s"
-        conditions_attr = self.__dict__
-        [conditions_attr.pop(field) for field, value in conditions_attr.copy().items() if value == None]
-        args = tuple(conditions_attr.values())
-        condition_format = self.generate_conditions(conditions_attr)
-        returning_cols = "returning " + " , ".join(cols) if cols else ""
-        query = f"""
-            delete from {table_name} {condition_format} {returning_cols}"""
-        self.cur.execute(query, args)
-        self.con.commit()
-        if cols:
-            users = self.to_dict()
-            if users:
-                objects = []
-                for i in users:
-                    objects.append(self.__class__(**i))
-                return objects
-
-    def update(self, **conditions):
-        table_name = self.__class__.__name__.lower() + "s"
-        cols = self.__dict__
-        [cols.pop(field) for field, value in cols.copy().items() if value == None]
-        set_cols = " = %s , ".join(cols.keys()) + " = %s"
-        condition_format = " = %s and ".join(conditions.keys()) + " = %s  "
-        args = tuple(list(cols.values()) + list(conditions.values()))
-        query = f"""
-            update {table_name} set {set_cols} where {condition_format} returning *
-        """
-        self.cur.execute(query, args)
-        self.con.commit()
-        users = self.to_dict()
-        if users:
-            objects = []
-            for i in users:
-                objects.append(self.__class__(**i))
-            return objects
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO, stream=sys.stdout)
+    asyncio.run(main())
